1cf84859cdc7acd21d8e719a055ef98712b61f71
elenamii <elenagholami@gmx.de>
2026-01-13 23:09:11 +0100

    floyd warshall am start

---
+++ src/aufgabe5_graphen/floyd_warshall.py
@@ -0,0 +1,29 @@
+from utils.graphen_inout import read_graph
+from utils.matrix_inout import print_matrix
+
+def floyd_warshall(G):
+    n = len(G)
+    D = [[float("inf") if G[i][j] == "∞" else G[i][j] for j in range(n)] for i in range(n)]
+    R = [[j if G[i][j] != "∞" else None for j in range(n)] for i in range(n)]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if D[i][k] + D[k][j] < D[i][j]:
+                    D[i][j] = D[i][k] + D[k][j]
+                    R[i][j] = R[i][k]
+    return D, R
+
+
+def run():
+    n = int(input("Anzahl Knoten: "))
+    G = read_graph(n)
+    D, R = floyd_warshall(G)
+    print("Distanzmatrix:")
+    print_matrix(D)
+    print("Routingmatrix:")
+    print(R)
+
+
+if __name__ == "__main__":
+    run()
