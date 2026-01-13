273104c2d15af7ee37559d5d9c0206bde18d6d9f
elenamii <elenagholami@gmx.de>
2026-01-13 11:21:35 +0100

    created gloabl multiply function

--- src/aufgabe1_matrix_mult/matrix_multiplication.py
+++ src/aufgabe1_matrix_mult/matrix_multiplication.py
@@ -1,53 +1,70 @@
 #Erstellen Sie ein Programm zur Multiplikation von Matrizen.
 #A x B = C
 # rows x cols
+# A (mxn) x B(nxp) = C (mxp)
+
+# reine rechenfunktion für Aufgabe 2-5 nutzbar
+def multiply(A, B):
+    rowsA =len(A)
+    colsA = len(A[0])
+    rowsB = len(B)
+    colsB = len (B[0])
+
+    if colsA != rowsB:
+        raise ValueError("Ungültige Dimensionen: Spalten(A) entsprechen nicht Zeilen(B)")
+    
+    C = []
+    for i in range(rowsA):
+        row = []
+        for j in range(colsB):
+            summe = 0
+            for k in range(colsA):
+                summe += A[i][k] * B[k][j]
+            row.append(summe)
+        C.append(row)
+
+    #   für jede Zeile:
+    #     neue Zeile
+    #     für jede Spalte:
+    #         Wert berechnen
+    #         Wert in Zeile speichern
+    #     Zeile in Ergebnis speichern  
+
+    return C
 
 
-print('Geben Sie die Dimensionen der Matrizen ein:')
-rowsA = int(input('Zeilen Matrix A:'))
-colsA = int(input('Spalten Matrix A:'))
+def run():
+    print("Matrixmultiplikation A x B")
+    print('Geben Sie die Dimensionen der Matrizen ein:')
+    rowsA = int(input('Zeilen Matrix A:'))
+    colsA = int(input('Spalten Matrix A:'))
 
-rowsB = int(input('Zeilen Matrix B:'))
-colsB = int(input('Spalten Matrix B:'))
+    rowsB = int(input('Zeilen Matrix B:'))
+    colsB = int(input('Spalten Matrix B:'))
 
-if (colsA != rowsB):
-    print ('Ungültige Dimensionen.Fehler: Anzahl Spalten von Matrix A muss gleich Anzahl Zeilen von Matrix B sein.')
-    exit()
+    if (colsA != rowsB):
+        print ('Ungültige Dimensionen. Fehler: Anzahl Spalten von Matrix A muss gleich Anzahl Zeilen von Matrix B sein.')
+        exit()
 
-A = []
-print ('Matrix A:')
-for i in range(rowsA):
-    row = []
-    for j in range (colsA):
-        row.append(int(input(f"A[{i}][{j}]: ")))
-    A.append(row)
+    A = []
+    print ('Matrix A:')
+    for i in range(rowsA):
+        row = []
+        for j in range (colsA):
+            row.append(int(input(f"A[{i}][{j}]: ")))
+        A.append(row)
 
-B= []
-print ('Matrix B:')
-for i in range (rowsB):
-    row= []
-    for j in range (colsB):
-        row.append(int(input(f"B[{i}][{j}]:")))
-    B.append(row)
+    B= []
+    print ('Matrix B:')
+    for i in range (rowsB):
+        row= []
+        for j in range (colsB):
+            row.append(int(input(f"B[{i}][{j}]:")))
+        B.append(row)
 
 
-C = []
-for i in range(rowsA):
-    row = []
-    for j in range(colsB):
-        summe = 0
-        for k in range(colsA):
-            summe += A[i][k] * B[k][j]
-        row.append(summe)
-    C.append(row)
 
-#   für jede Zeile:
-#     neue Zeile
-#     für jede Spalte:
-#         Wert berechnen
-#         Wert in Zeile speichern
-#     Zeile in Ergebnis speichern  
 
-print("Ergebnis-Matrix:")
-for row in C:
-    print(row)
+    print("Ergebnis-Matrix:")
+    for row in C:
+        print(row)
